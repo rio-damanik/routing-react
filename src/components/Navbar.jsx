5c5a5001eddfad9373625710d01cdcac5513e7e2
rio-damanik <168444216+rio-damanik@users.noreply.github.com>
2024-08-23 10:55:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My Website</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
